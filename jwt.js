b159d2976609a865342bb9ad44719a0112c7e75a
Ethan Arrowood <ethan.arrowood@gmail.com>
2018-01-16 00:46:14 -0500

    complete rebuild

--- jwt.js
+++ jwt.js
@@ -1,61 +1,106 @@
-'use strict'
-
 const fp = require('fastify-plugin')
 const JWT = require('jsonwebtoken')
 const assert = require('assert')
+const waterfall = require('async/waterfall')
 
-function fastifyJWT (fastify, opts, next) {
-  if (!opts.secret) {
-    return next(new Error('missing secret'))
-  }
+function isFunction(object) {
+  return Object.prototype.toString.call(object) === '[object Function]'
+}
 
-  const secret = opts.secret
-
-  fastify.decorate('jwt', {
-    sign: sign,
-    verify: verify,
-    decode: decode,
-    secret: secret
-  })
-
-  next()
-
-  function sign (payload, options, callback) {
-    assert(payload, 'missing payload')
-    options = options || {}
-    if (typeof options === 'function') {
-      callback = options
-      options = {}
-    }
-
-    if (typeof callback === 'function') {
-      JWT.sign(payload, secret, options, callback)
-    } else {
-      return JWT.sign(payload, secret, options)
-    }
-  }
-
-  function verify (token, options, callback) {
-    assert(token, 'missing token')
-    assert(secret, 'missing secret')
-    options = options || {}
-    if (typeof options === 'function') {
-      callback = options
-      options = {}
-    }
-
-    if (typeof callback === 'function') {
-      JWT.verify(token, secret, options, callback)
-    } else {
-      return JWT.verify(token, secret, options)
-    }
-  }
-
-  function decode (token, options) {
-    assert(token, 'missing token')
-    options = options || {}
-    return JWT.decode(token, options)
+function wrapStaticSecretInCallback(secret){
+  return function(_, __, cb) { // _ = req, __ = res
+    return cb(null, secret) 
   }
 }
 
-module.exports = fp(fastifyJWT, '>=0.13.1')
+function fastifyJwt(fastify, options, next) {
+  if (!options.secret) {
+    return next(new Error('missing secret'))
+  }
+
+  let secretCallback = options.secret
+
+  if(!isFunction(secretCallback)) 
+    secretCallback = wrapStaticSecretInCallback(secretCallback)
+  
+  let _requestProperty = options.userProperty || options.requestProperty || 'user'
+  let _resultProperty = options.resultProperty
+  const credentialsRequired = typeof options.credentialsRequired === 'undefined' ? true : options.credentialsRequired
+  
+  fastify.decorate('jwt', {
+    sign: sign,
+    verify: verify,
+  })
+
+  next() 
+
+  function sign (request, reply, next) {
+
+    JWT.sign(request.body, secret, options, callback)
+  } // end sign 
+
+  function verify (request, reply, next) {
+    let token
+
+    if (request.headers && request.headers.authorization) {
+      const parts = request.headers.authorization.split(' ')
+      if (parts.length === 2) {
+        const scheme = parts[0]
+        const credentials = parts[1]
+
+        if (/^Bearer$/i.test(scheme)) {
+          token = credentials
+        } else {
+          if (credentialsRequired) {
+            return next(new Error('Format is Authorization: Bearer [token]'))
+          } else {
+            return next()
+          }
+        }
+      } else {
+        return next(new Error('Format is Authorization: Bearer [token]'))
+      }
+    }
+
+    if (!token) {
+      if (credentialsRequired) {
+        return next(new Error('No authorization token was found'))
+      } else {
+        return next()
+      }
+    }
+
+    let decoded_token
+
+    try {
+      decoded_token = JWT.decode(token, { complete: true }) || {}
+    } catch(err) {
+      return next(new Error(`invalid_token ${err}`))
+    }
+
+    waterfall([
+      function getSecret(callback) {
+        secretCallback(request, decoded_token, callback)
+      },
+      function verify(secret, callback) {
+        JWT.verify(token, secret, options, function(err, decoded) {
+          if (err) {
+            callback(err)
+          } else {
+            callback(null, decoded)
+          }
+        })
+      }
+    ], function (err, result) {
+      if (err) { return next(err) }
+      if (_resultProperty) {
+        reply[_resultProperty] = result
+      } else {
+        request[_requestProperty] = result
+      }
+      next()
+    })
+  } // end verify
+}
+
+module.exports = fp(fastifyJwt, '0.x')
