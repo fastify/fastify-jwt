c946a6f5635cef6298d9b922f2f5c01fd61d26c2
darkgl0w <31093081+darkgl0w@users.noreply.github.com>
2018-10-17 18:13:02 +0200

    Add more tests.
    Now covers even RSA private/public key pairs protected by a passphrase.

--- test.js
+++ test.js
@@ -10,6 +10,10 @@ const jwt = require('./jwt')
 const publicKey = fs.readFileSync(`${path.join(__dirname, 'certs')}/public.key`, 'utf8')
 const privateKey = fs.readFileSync(`${path.join(__dirname, 'certs')}/private.key`, 'utf8')
 
+const publicKeyProtected = fs.readFileSync(`${path.join(__dirname, 'certs')}/public.pem`)
+// passphrase used to protect the private key: super secret passphrase
+const privateKeyProtected = fs.readFileSync(`${path.join(__dirname, 'certs')}/private.pem`)
+
 test('register', function (t) {
   t.plan(7)
 
@@ -40,7 +44,12 @@ test('register', function (t) {
   t.test('secret as an object', function (t) {
     t.plan(1)
     const fastify = Fastify()
-    fastify.register(jwt, { secret: { key: privateKey, passphrase: publicKey } }).ready(function (error) {
+    fastify.register(jwt, {
+      secret: {
+        private: privateKeyProtected,
+        public: 'super secret passphrase'
+      }
+    }).ready(function (error) {
       t.is(error, null)
     })
   })
@@ -65,8 +74,8 @@ test('register', function (t) {
     const fastify = Fastify()
     fastify.register(jwt, {
       secret: {
-        key: privateKey,
-        passphrase: publicKey
+        private: privateKey,
+        public: publicKey
       },
       options: {
         issuer: 'Some issuer',
@@ -286,8 +295,8 @@ test('sign and verify with RSA and options', function (t) {
     const fastify = Fastify()
     fastify.register(jwt, {
       secret: {
-        key: privateKey,
-        passphrase: publicKey
+        private: privateKey,
+        public: publicKey
       },
       options: {
         issuer: 'Some issuer',
@@ -330,8 +339,152 @@ test('sign and verify with RSA and options', function (t) {
     const fastify = Fastify()
     fastify.register(jwt, {
       secret: {
-        key: privateKey,
-        passphrase: publicKey
+        private: privateKey,
+        public: publicKey
+      },
+      options: {
+        issuer: 'Some issuer',
+        subject: 'Some subject',
+        audience: 'Some audience',
+        algorithm: 'RS256'
+      }
+    })
+
+    fastify.post('/signSync', function (request, reply) {
+      reply.jwtSign(request.body)
+        .then(function (token) {
+          return reply.send({ token })
+        })
+    })
+
+    fastify.get('/verifySync', function (request, reply) {
+      request.jwtVerify()
+        .then(function (decodedToken) {
+          return reply.send(decodedToken)
+        })
+    })
+
+    fastify.post('/signAsync', function (request, reply) {
+      reply.jwtSign(request.body, function (error, token) {
+        return reply.send(error || { token })
+      })
+    })
+
+    fastify.get('/verifyAsync', function (request, reply) {
+      request.jwtVerify(function (error, decodedToken) {
+        return reply.send(error || decodedToken)
+      })
+    })
+
+    fastify
+      .ready()
+      .then(function () {
+        t.test('synchronous', function (t) {
+          t.plan(2)
+
+          fastify.inject({
+            method: 'post',
+            url: '/signSync',
+            payload: { foo: 'bar' }
+          }).then(function (signResponse) {
+            const token = JSON.parse(signResponse.payload).token
+            t.ok(token)
+
+            fastify.inject({
+              method: 'get',
+              url: '/verifySync',
+              headers: {
+                authorization: `Bearer ${token}`
+              }
+            }).then(function (verifyResponse) {
+              const decodedToken = JSON.parse(verifyResponse.payload)
+              t.is(decodedToken.foo, 'bar')
+            })
+          })
+        })
+
+        t.test('with callbacks', function (t) {
+          t.plan(2)
+
+          fastify.inject({
+            method: 'post',
+            url: '/signAsync',
+            payload: { foo: 'bar' }
+          }).then(function (signResponse) {
+            const token = JSON.parse(signResponse.payload).token
+            t.ok(token)
+
+            fastify.inject({
+              method: 'get',
+              url: '/verifyAsync',
+              headers: {
+                authorization: `Bearer ${token}`
+              }
+            }).then(function (verifyResponse) {
+              const decodedToken = JSON.parse(verifyResponse.payload)
+              t.is(decodedToken.foo, 'bar')
+            })
+          })
+        })
+      })
+  })
+})
+
+test('sign and verify with RSA passphrase protected private key (PEM file) and options', function (t) {
+  t.plan(2)
+
+  t.test('server methods', function (t) {
+    t.plan(2)
+
+    const fastify = Fastify()
+    fastify.register(jwt, {
+      secret: {
+        private: { key: privateKeyProtected, passphrase: 'super secret passphrase' },
+        public: publicKeyProtected
+      },
+      options: {
+        issuer: 'Some issuer',
+        subject: 'Some subject',
+        audience: 'Some audience',
+        algorithm: 'RS256'
+      }
+    })
+
+    fastify
+      .ready()
+      .then(function () {
+        t.test('synchronous', function (t) {
+          t.plan(1)
+
+          const token = fastify.jwt.sign({ foo: 'bar' })
+          const decoded = fastify.jwt.verify(token)
+
+          t.is(decoded.foo, 'bar')
+        })
+
+        t.test('with callbacks', function (t) {
+          t.plan(3)
+
+          fastify.jwt.sign({ foo: 'bar' }, function (error, token) {
+            t.error(error)
+
+            fastify.jwt.verify(token, function (error, decoded) {
+              t.error(error)
+              t.is(decoded.foo, 'bar')
+            })
+          })
+        })
+      })
+  })
+
+  t.test('route methods', function (t) {
+    t.plan(2)
+
+    const fastify = Fastify()
+    fastify.register(jwt, {
+      secret: {
+        private: { key: privateKeyProtected, passphrase: 'super secret passphrase' },
+        public: publicKeyProtected
       },
       options: {
         issuer: 'Some issuer',
