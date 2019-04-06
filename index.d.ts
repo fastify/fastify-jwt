0c52866e86f2a5da5b9397befc22c4cb725415e9
3846masa <3846masahiro+git@gmail.com>
2019-04-06 11:58:33 +0900

    Format index.d.ts

--- index.d.ts
+++ index.d.ts
@@ -1,47 +1,43 @@
-import * as fastify from "fastify";
-import { IncomingMessage, Server, ServerResponse } from "http";
-import { DecodeOptions, Secret, SignOptions, VerifyCallback, VerifyOptions } from "jsonwebtoken";
+import * as fastify from 'fastify';
+import { IncomingMessage, Server, ServerResponse } from 'http';
+import { DecodeOptions, Secret, SignOptions, VerifyCallback, VerifyOptions } from 'jsonwebtoken';
 
-declare module "fastify"
-{
-    interface Jwt
-    {
-        decode: (token: string, options?: DecodeOptions) => null | { [key: string]: any } | string;
-        options: {
-            /**
-             * decodeOptions
-             */
-            decode: DecodeOptions,
+declare module 'fastify' {
+  interface Jwt {
+    decode: (token: string, options?: DecodeOptions) => null | { [key: string]: any } | string;
+    options: {
+      /**
+       * decodeOptions
+       */
+      decode: DecodeOptions;
 
-            /**
-             * signOptions
-             */
-            sign: SignOptions,
+      /**
+       * signOptions
+       */
+      sign: SignOptions;
 
-            /**
-             * verifyOptions
-             */
-            verify: VerifyOptions,
-        };
-        secret: Secret;
-        sign: (playload: string | Buffer | object, options?: SignOptions, callback?: VerifyCallback) => string;
-        verify: (token: string, options?: VerifyOptions, callback?: VerifyCallback) => Promise<object | string>;
-    }
-    interface FastifyInstance<HttpServer = Server, HttpRequest = IncomingMessage, HttpResponse = ServerResponse>
-    {
-        jwt: Jwt;
-    }
+      /**
+       * verifyOptions
+       */
+      verify: VerifyOptions;
+    };
+    secret: Secret;
+    sign: (playload: string | Buffer | object, options?: SignOptions, callback?: VerifyCallback) => string;
+    verify: (token: string, options?: VerifyOptions, callback?: VerifyCallback) => Promise<object | string>;
+  }
+  interface FastifyInstance<HttpServer = Server, HttpRequest = IncomingMessage, HttpResponse = ServerResponse> {
+    jwt: Jwt;
+  }
 
-    interface FastifyRequest<
-        HttpRequest,
-        Query = DefaultQuery,
-        Params = DefaultParams,
-        Headers = DefaultHeaders,
-        Body = DefaultBody
-        >
-    {
-        jwtVerify(callback?: VerifyCallback): Promise<any>;
-    }
+  interface FastifyRequest<
+    HttpRequest,
+    Query = DefaultQuery,
+    Params = DefaultParams,
+    Headers = DefaultHeaders,
+    Body = DefaultBody
+  > {
+    jwtVerify(callback?: VerifyCallback): Promise<any>;
+  }
 }
 
 declare const fastifyJWT: fastify.Plugin<Server, IncomingMessage, ServerResponse, { secret: Secret }>;
