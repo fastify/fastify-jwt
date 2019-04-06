567b1d56ec97c8cc734d897874dfb0ebcd12f480
3846masa <3846masahiro+git@gmail.com>
2019-04-06 13:24:45 +0900

    Update function types

--- index.d.ts
+++ index.d.ts
@@ -3,6 +3,16 @@ import * as http from 'http';
 import * as jwt from 'jsonwebtoken';
 
 declare module 'fastify' {
+  namespace JWTTypes {
+    type PayloadType = object | string;
+
+    interface SignCallback extends jwt.SignCallback {}
+
+    interface VerifyCallback<Decoded extends PayloadType> extends jwt.VerifyCallback {
+      (err: jwt.VerifyErrors, decoded: Decoded): void;
+    }
+  }
+
   interface JWT {
     options: {
       decode: jwt.DecodeOptions;
@@ -10,17 +20,39 @@ declare module 'fastify' {
       verify: jwt.VerifyOptions;
     };
     secret: jwt.Secret;
-    sign: (payload: string | Buffer | object, options?: jwt.SignOptions, callback?: jwt.VerifyCallback) => string;
-    verify: (token: string, options?: jwt.VerifyOptions, callback?: jwt.VerifyCallback) => Promise<object | string>;
-    decode: (token: string, options?: jwt.DecodeOptions) => null | { [key: string]: any } | string;
+
+    sign(payload: JWTTypes.PayloadType | Buffer, options?: jwt.SignOptions): string;
+    sign(payload: JWTTypes.PayloadType | Buffer, callback: JWTTypes.SignCallback): void;
+    sign(payload: JWTTypes.PayloadType | Buffer, options: jwt.SignOptions, callback: JWTTypes.SignCallback): void;
+
+    verify<Decoded extends JWTTypes.PayloadType = any>(token: string, options?: jwt.VerifyOptions): Decoded;
+    verify<Decoded extends JWTTypes.PayloadType = any>(token: string, callback: JWTTypes.VerifyCallback<Decoded>): void;
+    verify<Decoded extends JWTTypes.PayloadType = any>(
+      token: string,
+      options: jwt.VerifyOptions,
+      callback: JWTTypes.VerifyCallback<Decoded>,
+    ): void;
+
+    decode<Decoded extends JWTTypes.PayloadType = any>(token: string, options?: jwt.DecodeOptions): null | Decoded;
   }
 
   interface FastifyInstance {
     jwt: JWT;
   }
 
+  interface FastifyReply<HttpResponse> {
+    jwtSign(payload: JWTTypes.PayloadType | Buffer, options?: jwt.SignOptions): Promise<string>;
+    jwtSign(payload: JWTTypes.PayloadType | Buffer, callback: JWTTypes.SignCallback): void;
+    jwtSign(payload: JWTTypes.PayloadType | Buffer, options: jwt.SignOptions, callback: JWTTypes.SignCallback): void;
+  }
+
   interface FastifyRequest<HttpRequest> {
-    jwtVerify(callback?: jwt.VerifyCallback): Promise<any>;
+    jwtVerify<Decoded extends JWTTypes.PayloadType = any>(options?: jwt.VerifyOptions): Promise<Decoded>;
+    jwtVerify<Decoded extends JWTTypes.PayloadType = any>(callback: JWTTypes.VerifyCallback<Decoded>): void;
+    jwtVerify<Decoded extends JWTTypes.PayloadType = any>(
+      options: jwt.VerifyOptions,
+      callback: JWTTypes.VerifyCallback<Decoded>,
+    ): void;
   }
 }
 
