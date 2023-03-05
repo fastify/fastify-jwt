0a65c24b3c8a5d778f953f289d4880305afd9cc0
Manuel Spigolon <behemoth89@gmail.com>
2023-03-05 13:03:41 +0100

    chore: add async assertion

--- test/jwt.test.js
+++ test/jwt.test.js
@@ -1387,13 +1387,15 @@ test('sign and verify with trusted token', function (t) {
   })
 
   t.test('Trusted token - async verification', function (t) {
-    t.plan(1)
+    t.plan(2)
 
     const f = Fastify()
     f.register(jwt, { secret: 'test', trusted: (request, { jti }) => Promise.resolve(jti !== 'untrusted') })
     f.get('/', (request, reply) => {
       request.jwtVerify()
         .then(function (decodedToken) {
+          delete decodedToken?.iat
+          t.same(decodedToken, { foo: 'bar', jti: 'trusted' })
           return reply.send(decodedToken)
         })
         .catch(function (error) {
