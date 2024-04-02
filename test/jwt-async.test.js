89a496482a44cfbfb233b5dd755188413985b15f
NikitaFedorov1 <nikitaf@piazzaro.io>
2024-04-02 22:57:45 +0200

    test: Async key provider errors should be resolved internaly

--- test/jwt-async.test.js
+++ test/jwt-async.test.js
@@ -33,3 +33,30 @@ test('Async key provider should be resolved internaly', async function (t) {
   t.comment("Should be 'undefined'")
   t.equal(response.payload, 'function')
 })
+
+test('Async key provider errors should be resolved internaly', async function (t) {
+  const fastify = Fastify()
+  fastify.register(jwt, {
+    secret: {
+      private: 'supersecret',
+      public: async () => false
+    },
+    verify: {
+      extractToken: (request) => request.headers.jwt,
+      key: () => Promise.resolve('supersecret')
+    }
+  })
+  fastify.get('/', async function (request, reply) {
+    request.headers.jwt =
+      'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c'
+    await request.jwtVerify()
+    return reply.send(typeof request.user.then)
+  })
+  const response = await fastify.inject({
+    method: 'get',
+    url: '/'
+  })
+
+  t.comment('Should be 401')
+  t.equal(response.statusCode, 500)
+})
