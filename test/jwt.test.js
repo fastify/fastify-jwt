d779849eaaac0e2cf6ca8f266c54643deb4ec952
Frederik Prijck <frederik.prijck@gmail.com>
2025-03-06 00:11:50 +0100

    Update jwt.test.js
    Signed-off-by: Frederik Prijck <frederik.prijck@gmail.com>

--- test/jwt.test.js
+++ test/jwt.test.js
@@ -1420,7 +1420,7 @@ test('decode', async function (t) {
 })
 
 test('errors', async function (t) {
-  t.plan(15)
+  t.plan(16)
 
   const fastify = Fastify()
   fastify.register(jwt, {
@@ -1735,6 +1735,33 @@ test('errors', async function (t) {
 
     const error = JSON.parse(verifyResponse.payload)
     t.assert.strictEqual(error.message, 'Authorization token is invalid: The iss claim value is not allowed.')
+    t.assert.strictEqual(verifyResponse.statusCode, 401)
+  })
+
+  await t.test('requestVerify function: wrap missing required claims', async function (t) {
+    t.plan(3)
+
+    const signResponse = await fastify.inject({
+      method: 'post',
+      url: '/sign',
+      payload: {
+        payload: { foo: 'bar' }
+      }
+    })
+
+    const token = JSON.parse(signResponse.payload).token
+    t.assert.ok(token)
+
+    const verifyResponse = await fastify.inject({
+      method: 'get',
+      url: '/verifyFailMissingRequiredClaim',
+      headers: {
+        authorization: `Bearer ${token}`
+      }
+    })
+
+    const error = JSON.parse(verifyResponse.payload)
+    t.assert.strictEqual(error.message, 'Authorization token is invalid: The bar claim is required.')
     t.assert.strictEqual(verifyResponse.statusCode, 401)
   })
 
